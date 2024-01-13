a2dd4c7e854bff6d94ba638d6c73535a5122766c
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-01-13 02:47:03 +0100

    code coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -4,16 +4,18 @@ import Greet from "./greet";
 
 // Test Driven Development: Test is written before the actual code.
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
+describe("Greet", () => {
+  test("Renders correctly", () => {
+    render(<Greet />);
 
-  const textEl = screen.getByText(/hello/i);
-  expect(textEl).toBeInTheDocument();
-});
+    const textEl = screen.getByText(/hello/i);
+    expect(textEl).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Jay" />);
+  test("Renders with a name", () => {
+    render(<Greet name="Jay" />);
 
-  const textEl = screen.getByText(/hello jay/i);
-  expect(textEl).toBeInTheDocument();
+    const textEl = screen.getByText(/hello jay/i);
+    expect(textEl).toBeInTheDocument();
+  });
 });
--- src/test/__ mocks __/fileMock.js
+++ src/test/__ mocks __/fileMock.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-
 module.exports = {
   __esModule: true,
   default: "test-file-stub",
